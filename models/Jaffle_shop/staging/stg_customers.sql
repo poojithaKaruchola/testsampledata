7ac9e9d8f431d996bcb4b11406b7729f0f0506f5
poojitha K <poojitha.karuchola@cognizant.com>
2023-05-17 04:24:10 +0000

    commited on 17/05/2023

---
+++ models/Jaffle_shop/staging/stg_customers.sql
@@ -0,0 +1,9 @@
+select
+    id as customer_id,
+    first_name,
+    last_name
+
+from {{ env_var("DBT_SOURCE_DATABASE", "RAW") }}.jaffle_shop.customers
+--where customer_id > {{ var('CustID','20')}}
+
+
